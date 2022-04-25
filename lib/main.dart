3e406576a4eb3b6fc53e0fb4d0a5038d6df7832d
jay <toti051011@gmail.com>
2022-04-25 10:56:27 +0700

    menambahkan member_profile.dart, membuat theme.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'pages/home_page.dart';
+import 'pages/member_profile.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        fontFamily: 'Poppins'
+        fontFamily: 'Poppins',
       ),
       home: MemberProfile(),
     );
---
+++ lib/pages/member_profile.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MemberProfile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlue,
+      body: Text('TESTING', style: TextStyle(
+          fontSize: 40
+      ),),
+    );
+  }
+}
